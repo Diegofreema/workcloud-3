ce76fa9d8dcdcf62d510271114fb97ed0673f4ed
Diego <diegofreeman78@gmail.com>
2024-02-19 23:02:14 +0100

    ui updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,7 +9,7 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import * as Updates from 'expo-updates';
-
+import * as SecureStore from 'expo-secure-store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { PaperProvider } from 'react-native-paper';
 import Toast from 'react-native-toast-message';
@@ -18,6 +18,24 @@ import { config } from '@gluestack-ui/config';
 import { useDarkMode } from '@/hooks/useDarkMode';
 import { StatusBar } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { ClerkProvider } from '@clerk/clerk-expo';
+const ClerkKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
@@ -74,14 +92,16 @@ export default function RootLayout() {
   }
 
   return (
-    <GluestackUIProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <PaperProvider>
-          <RootLayoutNav />
-          <Toast />
-        </PaperProvider>
-      </QueryClientProvider>
-    </GluestackUIProvider>
+    <ClerkProvider publishableKey={ClerkKey} tokenCache={tokenCache}>
+      <GluestackUIProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <PaperProvider>
+            <RootLayoutNav />
+            <Toast />
+          </PaperProvider>
+        </QueryClientProvider>
+      </GluestackUIProvider>
+    </ClerkProvider>
   );
 }
 
